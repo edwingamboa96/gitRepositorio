144c3b1622d55c1ddff74750564b340e5096e42e
edwin gamboa <edwin.gamboa96@gmail.com>
2020-02-13 16:53:08 -0500

    Se agregan valore demo y metodos basicos

--- order-service/src/main/java/com/geekshirt/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/geekshirt/orderservice/controller/OrderController.java
@@ -1,11 +1,15 @@
 package com.geekshirt.orderservice.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.geekshirt.orderservice.dto.OrderResponse;
@@ -16,11 +20,59 @@ public class OrderController {
 	@GetMapping(value = "order")
 	public ResponseEntity<List<OrderResponse>> finAll() {
 		List<OrderResponse> orderList = new ArrayList<OrderResponse>();
+		OrderResponse order=new OrderResponse();
+		order.setAccountId("696396336");
+		order.setOrderId("123456");
+		order.setStatus("Pending");
+		order.setTotalAmount(5.6);
+		order.setTotalAmount(56.69);
+		order.setTotalTax(5.6);
+		order.setTransactionDate(new Date());
 		
-		return new ResponseEntity<>(orderList, HttpStatus.OK);
-        
+		OrderResponse order2=new OrderResponse();
+		order2.setAccountId("54156647");
+		order2.setOrderId("44444");
+		order2.setStatus("OK");
+		order2.setTotalAmount(3.6);
+		order2.setTotalAmount(5.3);
+		order2.setTotalTax(58.3);
+		order2.setTransactionDate(new Date());
+		
+		orderList.add(order2);
+		orderList.add(order);
+		
+		return new ResponseEntity<>(orderList, HttpStatus.OK);     
+	}
+	
+	@GetMapping(value ="order/{orderId}" )
+	public ResponseEntity<OrderResponse>getByID(@PathVariable String orderId){
+		OrderResponse order2=new OrderResponse();
+		order2.setAccountId("432453");
+		order2.setOrderId(orderId);
+		order2.setStatus("OK");
+		order2.setTotalAmount(3.6);
+		order2.setTotalAmount(5.3);
+		order2.setTotalTax(58.3);
+		order2.setTransactionDate(new Date());		
+		
+		return  new ResponseEntity<>(order2,HttpStatus.CREATED);
 		
 		
 	}
+	
+	@PostMapping(value = "order/create")
+	public ResponseEntity<OrderResponse>createOrder(@RequestBody OrderRequest payload){
+		OrderResponse order2=new OrderResponse();
+		order2.setAccountId(payload.getAccountId());
+		order2.setOrderId("44444");
+		order2.setStatus("OK");
+		order2.setTotalAmount(3.6);
+		order2.setTotalAmount(5.3);
+		order2.setTotalTax(58.3);
+		order2.setTransactionDate(new Date());	
+		
+		return  new ResponseEntity<>(order2,HttpStatus.OK);
+	}
+	
 
 }
---
+++ order-service/src/main/java/com/geekshirt/orderservice/controller/OrderRequest.java
@@ -0,0 +1,11 @@
+package com.geekshirt.orderservice.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderRequest {
+	private String accountId;
+
+}
